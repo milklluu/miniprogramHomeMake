4668d49a446cbf243cdebaaf5886b2e105a31ea0
Lucy Wang <190928386@qq.com>
2021-06-04 15:38:46 +0800

    wxs works

--- components/tabs/tabs.js
+++ components/tabs/tabs.js
@@ -27,6 +27,9 @@ Component({
     handleTabChange:function(event){
   
       const index=event.currentTarget.dataset.index
+      if(index=== this.data.currentTabIndex){
+        return
+      }
       this.setData({
         currentTabIndex:index
       })
@@ -34,7 +37,22 @@ Component({
       this.triggerEvent('change', {index})
     },
     handleTouchMove:function(event){
-      console.log(event)
+      const direction=event.direction
+      const currentTabIndex=this.data.currentTabIndex
+      const targetTabIndex=currentTabIndex+direction
+      if(targetTabIndex<0 || targetTabIndex>this.data.tabs.length-1)
+      {
+        return
+      }
+      const customEvent={
+        currentTarget:{
+          dataset:{
+            index:targetTabIndex
+          }
+        }
+      }
+      this.handleTabChange(customEvent)
+      //console.log(event)
     }
 
   }
